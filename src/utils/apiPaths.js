badad182cd3077d8ef7534f97ace22e5316a146f
shashank8565 <shashankrajput0992@gmail.com>
2025-05-28 00:25:24 +0530

    changingbackendurl

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://backend-expense-tracker-wheat.vercel.app/";
 
 // utils/apiPaths.js
 export const API_PATHS = {
@@ -10,16 +10,16 @@ export const API_PATHS = {
   DASHBOARD: {
     GET_DATA: "/api/v1/dashboard",
   },
-  INCOME:{
+  INCOME: {
     ADD_INCOME: "/api/v1/income/add",
     GET_ALL_INCOME: "/api/v1/income/get",
-    DELETE_INCOME: (incomeId) =>  `/api/v1/income/${incomeId}`,
+    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
     DOWNLOAD_INCOME: `/api/v1/income/downloadexcel`,
   },
-  EXPENSE:{
+  EXPENSE: {
     ADD_EXPENSE: "/api/v1/expense/add",
     GET_ALL_EXPENSE: "/api/v1/expense/get",
-    DELETE_EXPENSE: (expenseId) =>  `/api/v1/expense/${expenseId}`,
+    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
     DOWNLOAD_EXPENSE: `/api/v1/expense/downloadexcel`,
   },
   IMAGE: {
